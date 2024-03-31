69573f1e8fc079ceb1e5db806c61a6684c16d88f
rttrindade <rttrindade93@gmail.com>
2024-03-31 12:46:01 -0300

    Primeira integração com MySql, adicionado pagina de admin ao projeto

--- app_localiza_pet/models.py
+++ app_localiza_pet/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class PostTeste(models.Model):
+    titulo = models.CharField(max_length=250)
+    conteudo = models.TextField()
--- projeto_localiza_pet/urls.py
+++ projeto_localiza_pet/urls.py
@@ -1,11 +1,9 @@
-# from django.contrib import admin
-
+from django.contrib import admin
 from django.urls import path
-
 from app_localiza_pet import views
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 
     # url da primeira pagina de login
     path('', views.home, name='home'),
