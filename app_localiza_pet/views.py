bf907efa23ba925014bdca1a86d533f089ce0138
rttrindade <rttrindade93@gmail.com>
2024-04-10 16:32:37 -0300

    Criado função que autentica usuário

--- app_localiza_pet/views.py
+++ app_localiza_pet/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login as django_login
+from django.http import HttpResponseRedirect
+
 
 # Create your views here.
 
 
 def login(request):
+
+    if request.method == 'GET':
+        return render(request, 'login.html')
+
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+
+    user = authenticate(username=username, password=password)
+
+    if user:
+        django_login(request, user)
+        return HttpResponseRedirect('/feed/')
     return render(request, 'login.html')
 
 
