80c03e8bb9e6df5c85bdcb0a79933a01784fcbce
rttrindade <rttrindade93@gmail.com>
2024-04-16 20:46:05 -0300

    - adicionado pagina de cadastro de usuario - adicionado função de cadastro do usuario - adicionado proteção de pagina com login - adicionado redirecionamento da entrada para o login - adicionado função de logout - alterado htmls do login para exibir alerta do "senha invalida" - alterado html do feed para redirecionamento do login e logout

--- app_localiza_pet/views.py
+++ app_localiza_pet/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login as django_login
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as django_login, logout as django_logout
 from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 
 # Create your views here.
+
+def entrada(request):
+    return redirect('/login/')
 
 
 def login(request):
@@ -19,12 +24,39 @@ def login(request):
     if user:
         django_login(request, user)
         return HttpResponseRedirect('/feed/')
-    return render(request, 'login.html')
+    msg = 'Usuario e(ou) senha invalidos'
+    return render(request, 'login.html', {'msg': msg})
 
 
+@login_required(login_url='/login/')
 def feed(request):
     return render(request, 'feed.html')
 
 
+@login_required(login_url='/login/')
 def post(request):
     return render(request, 'teste/post.html')
+
+
+@login_required(login_url='/login/')
+def logout(request):
+    django_logout(request)
+    return redirect('/login/')
+
+
+def cadastro(request):
+    if request.method == 'GET':
+        return render(request, 'cadastro.html')
+
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+
+    user = authenticate(username=username, password=password)
+    if user:
+        return redirect('/login/')
+
+    User.objects.create_user(
+        username=request.POST.get('username'),
+        password=request.POST.get('password'),
+    )
+    return render(request, 'login.html')
--- projeto_localiza_pet/urls.py
+++ projeto_localiza_pet/urls.py
@@ -6,7 +6,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # url da primeira pagina de login
-    path('', views.login, name='login'),
+    path('', views.entrada, name='entrada'),
+    path('login/', views.login, name='login'),
     path('feed/', views.feed, name='feed'),
     path('post/', views.post, name='post'),
+    path('cadastro/', views.cadastro, name='cadastro'),
+    path('logout/', views.logout),
+
 ]
